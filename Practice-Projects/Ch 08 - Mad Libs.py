af612751901247e3353c72e9fe96010fe34e5773
Andrés Mogollón <77457235+leonardo-source@users.noreply.github.com>
2022-09-23 16:32:52 -0500

    Update Ch 08 - Mad Libs.py
    I just want to say 1000 of thanks
    No tenía idea del tercer argumento para el (sub module). Estaba estancado y frustrado por no encontrar la solución a ese ejercicio. Hasta que vi tu código. quiero compartir un pequeño aporte que disminuye un par de líneas de código implementando un findall y un uso del for loop.
    again thank my friend

--- Practice-Projects/Ch 08 - Mad Libs.py
+++ Practice-Projects/Ch 08 - Mad Libs.py
@@ -13,33 +13,23 @@ mad_libs = mad_text.read()
 print(mad_libs)
 
 # Regex for adjective, noun, adverb, and verb
-adjective_regex = re.compile('ADJECTIVE')
-noun_regex = re.compile('NOUN')
-adverb_regex = re.compile('ADVERB')
-verb_regex = re.compile('VERB')
+def madLibTale(argumen1):
+    patron = re.compile(r'(ADJETIVE|NOUN|VERB|ADVERB)')
+    listOfWords = patron.findall(argumen1)
 
-# The program iterates through the sentence imported by the user and if it runs into an adjective,
-# noun, adverb, or verb it has the user input a word to replace these.
-# Each word is subbed with a new word until its complete. Note: I'm not sure if the book goes over
-# the third argument in sub. If you leave the third argument out, it will replace all matches.
-# However, you can add 1 as a third argument to replace only the first occurence. This is why the
-# while loop will loop continuously until there are no more matches.
-while adjective_regex.search(mad_libs) is not None:
-    adjective = input('Please enter an adjective:\n')
-    mad_libs = adjective_regex.sub(adjective, mad_libs, 1)
-while noun_regex.search(mad_libs) is not None:
-    noun = input('Please enter a noun:\n')
-    mad_libs = noun_regex.sub(noun, mad_libs, 1)
-while adverb_regex.search(mad_libs) is not None:
-    adverb = input('Please enter an adverb:\n')
-    mad_libs = adverb_regex.sub(adverb, mad_libs, 1)
-while verb_regex.search(mad_libs) is not None:
-    verb = input('Please enter a verb:\n')
-    mad_libs = verb_regex.sub(verb, mad_libs, 1)
+    for i in range(len(listOfWords)):
+        patronToSub = re.compile(listOfWords[i])
+        print('Enter a(an) %s:' %(listOfWords[i]))
+        word = input()
+        argumen1 = patronToSub.sub(word, argumen1, 1)
+    print(argumen1)
+    return(argumen1)
+
+wroteTale = madLibTale(mad_libs)
 
 # Saves the mad_libs to a new text file
 new_text = open('new_mad_lib.txt', 'w')
-new_text.write(mad_libs)
+new_text.write(wroteTale)
 new_text.close()
 mad_text.close()
 print(mad_libs)
